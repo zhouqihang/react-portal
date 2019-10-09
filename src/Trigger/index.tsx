13299cb59298c1842265d4d35c0972f404d06a53
zhouqihang <810236916@qq.com>
2019-10-09 23:23:27 +0800

    trigger组件完善click触发条件下的显示隐藏逻辑

--- src/Trigger/index.tsx
+++ src/Trigger/index.tsx
@@ -5,9 +5,9 @@
  * @time 2019-09-29 13:52:06
  * @description Trigger,弹出层组件最外层调用
  */
-import React, { Component, CSSProperties, ReactHTMLElement, ReactElement, RefObject } from 'react';
-import classnames from 'classnames';
+import React, { Component, ReactHTMLElement, ReactElement, RefObject, createRef } from 'react';
 import Popup, { GetPositionType } from '../Popup';
+import { findDOMNode } from 'react-dom';
 
 const prefix = 'trigger';
 const TRIGGERS_ARR = ['click', 'focus', 'hover'];
@@ -15,22 +15,16 @@ export type TriggerType = 'click' | 'focus' | 'hover';
 
 
 interface ITriggerProps {
-    className?: string;
-    style?: CSSProperties;
     trigger?: TriggerType;
     popupContent?: React.ReactNode;
     getPopupPosition: GetPositionType;
     visible?: boolean;
     defaultVisible?: boolean;
     onChange?: (visible: boolean) => void;
-    // TODO
-    popupClassName?: string;
-    popupStyle?: CSSProperties;
 }
 interface ITriggerStates {
     visible: boolean;
 }
-
 
 class Trigger extends Component<ITriggerProps, ITriggerStates> {
     static defaultProps: Partial<ITriggerProps> = {
@@ -47,6 +41,13 @@ class Trigger extends Component<ITriggerProps, ITriggerStates> {
         return null;
     }
 
+    /**
+     * this timer is used for mouse event.
+     */
+    private timer: number | null = null;
+
+    private popupRef: RefObject<any> = createRef();
+
     constructor(props: ITriggerProps) {
         super(props);
         this.state = {
@@ -54,8 +55,57 @@ class Trigger extends Component<ITriggerProps, ITriggerStates> {
         }
     }
 
+    /**
+     * clearTimeout, remove document event listener
+     */
+    componentWillUnmount() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+        document.removeEventListener('click', this.handleDocumentClick);
+    }
+
+    /**
+     * when click the screen
+     * if click the trigger or popup, do nothing here
+     * 
+     */
+    handleDocumentClick = (e: MouseEvent) => {
+        if (this.props.trigger !== 'click') {
+            return;
+        }
+        let node = e.target;
+        let contains = false;
+        const popupNode = findDOMNode(this.popupRef.current);
+        const triggerNode = findDOMNode(this);
+        while (node && (node as HTMLElement).parentNode) {
+            if (node === popupNode || node === triggerNode) {
+                contains = true;
+                break;
+            }
+            node = (node as HTMLElement).parentNode;
+        }
+        if (!contains) {
+            this.togglePopupVisible(false);
+            this.props.onChange && this.props.onChange(false)
+        }
+    }
+
     togglePopupVisible = (visible: boolean) => {
-        this.setState({ visible })
+        this.setState({ visible }, () => {
+            // when trigger is click and visible is true, add the click event for document
+            // this will hidden popup when click the screen(not the popup self)
+            // when visible is false, remove the click event for document
+            if ('click' !== this.props.trigger) {
+                return;
+            }
+            if (true === visible) {
+                document.addEventListener('click', this.handleDocumentClick)
+            }
+            else {
+                document.removeEventListener('click', this.handleDocumentClick);
+            }
+        })
     }
 
     /**
@@ -68,6 +118,10 @@ class Trigger extends Component<ITriggerProps, ITriggerStates> {
      * @return {function} react event function
      */
     registerTriggerEvent = (eventName: string, visible?: boolean) => (e: ReactHTMLElement<any>) => {
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+
         const { children } = this.props;
 
         if (children && (children as ReactElement).props[eventName]) {
@@ -78,9 +132,18 @@ class Trigger extends Component<ITriggerProps, ITriggerStates> {
         if ('boolean' === typeof visible) {
             v = visible;
         }
-        this.togglePopupVisible(v);
 
-        this.props.onChange && this.props.onChange(v)
+        // timer will be clear when is already existed.
+        // when the visible state is the same, nothing todo
+        // else toggle the visible state
+        // this worked when mouse is moved between tooltip trigger and tooltip popup
+        if (this.state.visible === v) {
+            return;
+        }
+        this.timer = window.setTimeout(() => {
+            this.togglePopupVisible(v);
+            this.props.onChange && this.props.onChange(v)
+        }, 0);
     }
 
     /**
@@ -91,20 +154,31 @@ class Trigger extends Component<ITriggerProps, ITriggerStates> {
         if (!visible) {
             return null;
         }
-        const { popupContent, getPopupPosition } = this.props;
+        const { popupContent, getPopupPosition, trigger } = this.props;
+        let props: Record<string, any> = { ref: this.popupRef };
+        if ('hover' === trigger) {
+            props.onMouseEnter = this.registerTriggerEvent('onMouseEnter', true);
+            props.onMouseLeave = this.registerTriggerEvent('onMouseLeave', false);
+        }
+
+        let element = null;
+        if (React.isValidElement(popupContent)) {
+            element = React.cloneElement<any>(popupContent, props)
+        }
         return (
             <Popup
                 key="popup"
                 getPosition={getPopupPosition}
+                ref="a"
             >
-                {popupContent}
+                {element}
             </Popup>
         )
     }
 
     render() {
         // TODO 监测点击关闭的时候，判断事件触发链中是否包含popUp内容，如果不包括，则可以直接退出
-        const { className, style, children, trigger } = this.props;
+        const { children, trigger } = this.props;
 
         // get trigger element
         let triggerEle = null;
